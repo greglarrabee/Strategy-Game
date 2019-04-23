e3e7d4755934fc9f505729413eb635f6498cac66
caniscafall <ben.lees@lakesideschool.org>
2019-04-23 10:17:19 -0700

    Deleted github plugin, added new model

--- StrategyGame/Assets./Scripts/HexCoordinates.cs
+++ StrategyGame/Assets./Scripts/HexCoordinates.cs
@@ -40,18 +40,20 @@ public struct HexCoordinates
         return new HexCoordinates(x - z / 2, z);
     }
 
+    // Finds hex coordinates from a world position
     public static HexCoordinates FromPosition(Vector3 position)
     {
         // X-coordinate on grid depends on width of hexagons (inner radius)
         float x = position.x / (HexMetrics.innerRadius * 2f);
         // X up = Y down
         float y = -x;
-        // height of click determines Z-coord
+        // "height" of click determines Z-coord
         float offset = position.z / (HexMetrics.outerRadius * 3f);
         // Z up = X & Y down
         x -= offset;
         y -= offset;
 
+        // coordinates will eventually need to be integers
         int iX = Mathf.RoundToInt(x);
         int iY = Mathf.RoundToInt(y);
         int iZ = Mathf.RoundToInt(-x - y);
@@ -85,6 +87,37 @@ public struct HexCoordinates
         float x = (cX * 2 + cZ) * HexMetrics.innerRadius;
         float z = cZ * (1.5f * HexMetrics.outerRadius);
         return new Vector2(x, z);
+    }
+
+    // Returns a new HexCoordinates one tile away in the specified direction
+    // Directions start with due east and go clockwise
+    public static  HexCoordinates translate(HexCoordinates start, int dir)
+    {
+        HexCoordinates dXZ;
+        switch(dir)
+        {
+            case 0:
+                dXZ = new HexCoordinates(1, 0);
+                break;
+            case 1:
+                dXZ = new HexCoordinates(1, -1);
+                break;
+            case 2:
+                dXZ = new HexCoordinates(0, -1);
+                break;
+            case 3:
+                dXZ = new HexCoordinates(-1, 0);
+                break;
+            case 4:
+                dXZ = new HexCoordinates(-1, 1);
+                break;
+            case 5:
+                dXZ = new HexCoordinates(0, 1);
+                break;
+            default:
+                return start;
+        }
+        return new HexCoordinates(start.X + dXZ.X, start.Z + dXZ.Z);
     }
 
     // For internal debugging
