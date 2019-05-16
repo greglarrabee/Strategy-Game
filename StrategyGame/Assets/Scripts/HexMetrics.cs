daf87858b822d163c6d3248b55fa2b6f3b37f9ad
Greg Larrabee <gregoryl19@lakesideschool.org>
2019-05-16 08:47:31 -0700

    Changed cell blending to make the cell borders more clear

--- StrategyGame/Assets/Scripts/HexMetrics.cs
+++ StrategyGame/Assets/Scripts/HexMetrics.cs
@@ -8,6 +8,9 @@ public class HexMetrics
 
     public const float elevationStep = 5f;
 
+    public const float solidFactor = 0.75f;
+    public const float blendFactor = 1f - solidFactor;
+
     public static Vector3[] corners =
     {
          new Vector3(0f, 0f, outerRadius),
@@ -27,5 +30,21 @@ public class HexMetrics
     public static Vector3 GetSecondCorner(HexDirection direction)
     {
         return corners[(int)direction + 1];
+    }
+
+    public static Vector3 GetFirstSolidCorner(HexDirection direction)
+    {
+        return corners[(int)direction] * solidFactor;
+    }
+
+    public static Vector3 GetSecondSolidCorner(HexDirection direction)
+    {
+        return corners[(int)direction + 1] * solidFactor;
+    }
+
+    public static Vector3 GetBridge(HexDirection direction)
+    {
+        return (corners[(int)direction] + corners[(int)direction + 1]) *
+            0.5f * blendFactor;
     }
 }
